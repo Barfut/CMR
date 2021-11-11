59f1b039f6252c1f248ad9c70fb72033d24b37f8
DiegoSSanabria <diegosesaga@gmail.com>
2021-11-10 19:00:32 -0500

    Terminacion de ionventario en React

---
+++ src/components/contenidoInventario.jsx
@@ -0,0 +1,26 @@
+import ModalEditarAgregarVentas from "./modalEditarAgregarVentas";
+import ModalEditarInventario from "./modalEditarInventario";
+
+const ContenidoInventario = () => {
+    return (
+    
+        <tr>
+            <td>123123</td>
+            <td>Alimento Barf</td>
+            <td>Pollo - 250 gr</td>
+            <td>
+            <span>$</span>
+            55555
+            </td>
+            <td> Estado</td>
+            
+            <ModalEditarInventario />
+            
+        </tr>
+      
+
+    );
+  
+}
+
+export default ContenidoInventario
--- src/pages/administrarUsuarios.jsx
+++ src/pages/administrarUsuarios.jsx
@@ -11,12 +11,12 @@ function AdministrarUsuarios () {
     <div>
 
         <Layaut>
-            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 "> 
-                    <h1 class="h2 me-2"> Administra vendedores</h1>
-                    <div class="btn-toolbar mb-2 mb-md-0">
-                        <div class="btn-group me-2">
-                            <button type="button" class="btn btn-close-dark btn-sm "></button>
+            <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 "> 
+                    <h1 className="h2 me-2"> Administra vendedores</h1>
+                    <div className="btn-toolbar mb-2 mb-md-0">
+                        <div className="btn-group me-2">
+                            <button type="button" className="btn btn-close-dark btn-sm "></button>
                         </div>
                     </div>
                 </div> 
@@ -27,9 +27,9 @@ function AdministrarUsuarios () {
 
                 <br />
 
-                <div class="table-wrapper-scroll-y my-custom-scrollbar-resumen"> 
-                    <table class="table">
-                        <thead class="table-light sticky-top">
+                <div className="table-wrapper-scroll-y my-custom-scrollbar-resumen"> 
+                    <table className="table">
+                        <thead className="table-light sticky-top">
                         <tr>
                             <th width= "10%" scope="col">#</th>
                             <th width= "35%" scope="col">Nombre</th>
