753104e33133134cec41f9d0ad2a380b5b6a0ed8
DiegoSSanabria <diegosesaga@gmail.com>
2021-11-13 21:25:31 -0500

    boton filtro

--- src/pages/ventas.jsx
+++ src/pages/ventas.jsx
@@ -3,7 +3,8 @@ import React, { useEffect, useState, useRef } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Layaut from '../layouts/layaut';
-import { Dialog, Tooltip } from '@material-ui/core';
+import { Dialog, Tooltip } from '@mui/material';
+import axios from 'axios';
 
 const ventasBackend = [
   {
@@ -46,12 +47,38 @@ const Ventas = () => {
   const [ventas, setVentas] = useState([]);
   const [textoBoton, setTextoBoton] = useState('Crear Nuevo Vehículo');
   const [colorBoton, setColorBoton] = useState('indigo');
-
+  const [ejecutarConsulta, setEjecutarConsulta] = useState(true);
   
+  
+
+  const obtenerVentas = async () => {
+    const options = { method: 'GET', url: 'https://vast-waters-45728.herokuapp.com/vehicle/' };
+    await axios
+      .request(options)
+      .then(function (response) {
+        setVentas(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+    setEjecutarConsulta(false);
+  };
+
+  useEffect(() => {
+    console.log('consulta', ejecutarConsulta);
+    if (ejecutarConsulta) {
+      obtenerVentas();
+    }
+  }, [ejecutarConsulta]);
+
   useEffect(() => {
     //obtener lista de vehículos desde el backend
-    setVentas(ventasBackend);
-  }, []);
+    if (mostrarTabla) {
+      setEjecutarConsulta(true);
+    }
+  }, [mostrarTabla]);
+
+
 
   useEffect(() => {
     if (mostrarTabla) {
@@ -81,11 +108,7 @@ const Ventas = () => {
 
       </div>
       {mostrarTabla ? (
-        <TablaVentas 
-          setMostrarTabla={setMostrarTabla}
-          listaVentas={ventas}
-          setVentas={setVentas}
-        />
+        <TablaVentas listaVentas={ventas} setEjecutarConsulta={setEjecutarConsulta}/>
       ) : (
         <FormularioIngresarVenta
           setMostrarTabla={setMostrarTabla}
@@ -99,31 +122,33 @@ const Ventas = () => {
   );
 };
 
-const TablaVentas = ({ setMostrarTabla, listaVentas, setVentas }) => {
-  const form = useRef(null);
+const TablaVentas = ({listaVentas, setEjecutarConsulta}) => {
 
-  const submitEdit = (e) => {
-    e.preventDefault();
-    const fd = new FormData(form.current);
-    console.log(fd)
+  const [busqueda, setBusqueda] = useState('');
+  const [ventasFiltradas, setVentasFiltradas] = useState(listaVentas);
 
-    const nuevaEdicion = {};
-    fd.forEach((value, key) => {
-      nuevaEdicion[key] = value;
-    });
-    
-    setMostrarTabla(true);
-    setVentas([...listaVentas, nuevaEdicion]);
-    toast.success('Venta editada',{hideProgressBar: true});
+  useEffect(() => {
+    setVentasFiltradas(
+      listaVentas.filter((elemento) => {
+        return JSON.stringify(elemento).toLowerCase().includes(busqueda.toLowerCase());
+      })
+    );
+  }, [busqueda, listaVentas]);
 
-  }
   return (   
     <>
-    <h1 className=' fw-bold d-md-flex justify-content-start pt-3 pb-2 mb-3' >Registro de ventas</h1>
+    <div className='d-flex justify-content-start align-items-center'>
+      <h1 className=' fw-bold d-md-flex justify-content-start pe-5 pt-3 pb-2 mb-3' >Registro de ventas</h1>
+      <input
+        value={busqueda}
+        onChange={(e) => setBusqueda(e.target.value)}
+        placeholder='Buscar'
+        className='text-muted form-control w-25 h-50'
+      />
+    </div>
 
     <div className='table-wrapper-scroll-y my-custom-scrollbar-resumen'>
-        <form ref={form} onSubmit={submitEdit}>
-          <table className="table table-hover table-bordered">
+        <table className="table table-hover table-bordered">
             <thead className="table-light sticky-top ">
               <tr>
                   <th width="15%">Fecha</th>
@@ -137,39 +162,112 @@ const TablaVentas = ({ setMostrarTabla, listaVentas, setVentas }) => {
               </tr>
             </thead>
             <tbody>
-              {listaVentas.map((ventas) => {
+              {ventasFiltradas.map((ventas) => {
                   return (
-                <FilaVenta key = {nanoid()} ventas={ventas}/>
+                <FilaVenta 
+                  key = {nanoid()} 
+                  ventas={ventas}
+                  setEjecutarConsulta = {setEjecutarConsulta}
+                  />
                   );
                 })}
             </tbody>
           </table>
-        </form>
-    </div>
+      </div>
     </>
   );
 };
 
 
-const FilaVenta = ({ventas}) => {
+const FilaVenta = ({ventas, setEjecutarConsulta}) => {
   const [edit, setEdit] = useState(false)
  
+  const [infoNuevaVenta, setInfoNuevaVenta] = useState({
+    fecha: ventas.fecha,
+    cedula: ventas.cedula,
+    nombre: ventas.nombre,
+    producto: ventas.producto,
+    cantidad: ventas.cantidad,
+    valorUnitario: ventas.valorUnitario,
+  });
+
+  const actualizarVentas = async () => {
+    console.log(infoNuevaVenta);
+    //enviar la info al backend
+   
+    const options = {
+      method: 'PATCH',
+      url: 'https://vast-waters-45728.herokuapp.com/vehicle/update/',
+      headers: { 'Content-Type': 'application/json' },
+      data: { ...infoNuevaVenta, id: ventas._id },
+    };
+
+    await axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        toast.success('venta modificado con éxito');
+        setEdit(false);
+        setEjecutarConsulta(true);
+      })
+      .catch(function (error) {
+        toast.error('Error modificando la venta');
+        console.error(error);
+      });
+  };
+
+  const eliminarVenta = async () => {
+    const options = {
+      method: 'DELETE',
+      url: 'https://vast-waters-45728.herokuapp.com/vehicle/delete/',
+      headers: { 'Content-Type': 'application/json' },
+      data: { id: ventas._id },
+    };
+
+    await axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        toast.success('venta eliminado con éxito');
+        setEjecutarConsulta(true);
+      })
+      .catch(function (error) {
+        console.error(error);
+        toast.error('Error eliminando la venta');
+      });
+  };
+
   return (
 
     <tr >
       {edit ? (
         <>
           <td>
-            <input className='form-control m-0 p-0' type ='date' defaultValue={ventas.fecha}/>
+            <input className='form-control m-0 p-0' 
+            type ='date' 
+            value={infoNuevaVenta.fecha}
+            onChange={(e) => setInfoNuevaVenta({ ...infoNuevaVenta, fecha: e.target.value })}
+            />
           </td>
           <td>
-            <input className='form-control m-0 p-0' type ='number'defaultValue={ventas.cedula}/>
+            <input className='form-control m-0 p-0' 
+            type ='number'
+            value={infoNuevaVenta.cedula}
+            onChange={(e) => setInfoNuevaVenta({ ...infoNuevaVenta, cedula: e.target.value })}
+            />  
           </td>
           <td>
-            <input className='form-control m-0 p-0' type ='text' defaultValue={ventas.nombre}/>
+            <input className='form-control m-0 p-0' 
+            type ='text' 
+            value={infoNuevaVenta.nombre}
+            onChange={(e) => setInfoNuevaVenta({ ...infoNuevaVenta, nombre: e.target.value })}
+            />
           </td>
           <td>
-            <select className='form-select m-0 p-0' defaultValue={ventas.producto} >
+            <select className='form-select m-0 p-0' 
+            value={infoNuevaVenta.producto}
+            onChange={(e) => setInfoNuevaVenta({ ...infoNuevaVenta, producto: e.target.value })}
+            >
                 <optgroup label = "Alimento humedo"> 
                     <option>Alimento Barf / Pollo - 250 gr</option>
                     <option>Alimento Barf / Pollo - 500 gr</option>
@@ -187,10 +285,16 @@ const FilaVenta = ({ventas}) => {
             </select>     
           </td>
           <td>
-            <input min={1} max={250} className='form-control text-center m-0 p-0' type ='number'defaultValue={ventas.cantidad}/>
+            <input min={1} max={250} className='form-control text-center m-0 p-0' 
+            value={infoNuevaVenta.cantidad}
+            onChange={(e) => setInfoNuevaVenta({ ...infoNuevaVenta, cantidad: e.target.value })}
+            />
           </td>
           <td>
-            <input min={500} max={5000} className='form-control text-center m-0 p-0' type ='number'defaultValue={ventas.valorUnitario}/>
+            <input min={500} max={5000} className='form-control text-center m-0 p-0'
+            value={infoNuevaVenta.valorUnitario}
+            onChange={(e) => setInfoNuevaVenta({ ...infoNuevaVenta, valorUnitario: e.target.value })}
+            />
           </td>
         </>
 
@@ -209,19 +313,24 @@ const FilaVenta = ({ventas}) => {
         <td>
           <div className= 'd-flex justify-content-evenly'>
             {edit ? (
-              <button type='submit'>
+               <Tooltip title='Confirmar' arrow>
                 <i 
-                onClick={()=> setEdit(!edit)}
+                onClick={()=> actualizarVentas()}
                 className='fas fa-check btn-success shadow-none btn-sm '
                 />
-              </button>
+              </Tooltip>
             ):(
-              <i onClick={()=> setEdit(!edit)} 
-              className='fas fa-edit btn-outline-warning btn-sm'
-              />
+              <Tooltip title='Editar' arrow>
+                <i onClick={()=> setEdit(!edit)} 
+                className='fas fa-edit btn-outline-warning btn-sm'
+                />
+              </Tooltip>
             )}
-            <Tooltip title='Editar Vehículo' arrow>
-            <i className='fas fa-trash-alt btn-outline-danger btn-sm'></i>
+            <Tooltip title='Eliminar' arrow>
+              <i 
+                onClick={() => eliminarVenta()}
+                className='fas fa-trash-alt btn-outline-danger btn-sm'
+              />
             </Tooltip>
           </div>
         </td>
@@ -232,7 +341,7 @@ const FilaVenta = ({ventas}) => {
 const FormularioIngresarVenta = ({ setMostrarTabla, listaVentas, setVentas }) => {
   const form = useRef(null);
 
-  const submitForm = (e) => {
+  const submitForm = async (e) => {
     e.preventDefault();
     const fd = new FormData(form.current);
 
@@ -241,10 +350,32 @@ const FormularioIngresarVenta = ({ setMostrarTabla, listaVentas, setVentas }) =>
       nuevaVenta[key] = value;
     });
 
-    setMostrarTabla(true);
-    setVentas([...listaVentas, nuevaVenta]);
-    toast.success('Venta agregado',{hideProgressBar: true});
-    
+    const options = {
+      method: 'POST',
+      url: 'https://vast-waters-45728.herokuapp.com/vehicle/create',
+      headers: { 'Content-Type': 'application/json' },
+      data: { 
+        fecha: nuevaVenta.fecha,
+        cedula: nuevaVenta.cedula,
+        nombre: nuevaVenta.nombre,
+        producto: nuevaVenta.producto,
+        cantidad: nuevaVenta.cantidad,
+        valorUnitario: nuevaVenta.valorUnitario,
+       },
+    };
+  
+  await axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+      toast.success('Venta agregado con éxito');
+    })
+    .catch(function (error) {
+      console.error(error);
+      toast.error('Error creando una venta');
+    });
+
+  setMostrarTabla(true);
   };
 
   return (
